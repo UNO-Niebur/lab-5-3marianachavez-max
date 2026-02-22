d25d7b86df9c626809a2c4932a0cfcbecb0303df
3marianachavez-max <3marianachavez@gmail.com>
2026-02-22 21:49:44 +0000

    all done :)

--- LetterFrequency.py
+++ LetterFrequency.py
@@ -1,7 +1,7 @@
 #LetterFrequency.py
-#Name:
-#Date:
-#Assignment:
+#Name: Mariana Chavez
+#Date: 02.22.2026
+#Assignment: Lab 5
 
 #This program will create a CSV file of frequencies based on a text file.
 #Use Excel or similar spreadsheet software to visualize the frequencies of the CSV file.
@@ -18,7 +18,14 @@ def countLetters(message):
     #Find the position in the alphabet
     #Increase the frequency at that position. If position was 5, then frequencies[5] = frequencies[5] + 1
 
-
+    for letter in message:
+        if (alpha.find(letter) >= 0):
+            spot = alpha.find(letter)
+            freq[spot] = freq[spot] + 1
+            
+    #(I AM ASSUMING THAT THERE IS NO POSITION, SO I REMOVED THE PORTION THAT WAS ADDED IN THE VIDEO spot = 5, 
+    #freq[spot] = freq[spot] + 1, AND REPLACED IT WITH THE CODE ABOVE. OTHERWISE, "F" WAS COUNTED AS A 1 REGARDLESS
+    #IF IT WAS IN THE MESSAGE OR NOT.)
 
     #Create the output text in the format A,5\n if there were 5 letter A in the message.
     #Remember that the \n is the symbol for a new line.
--- WordGame.py
+++ WordGame.py
@@ -4,12 +4,16 @@ import random
 
 def inWord(letter, word):
     """Returns boolean if letter is anywhere in the given word"""
-
+    for ch in word:
+        if letter == ch:
+            return True
     return False
 
 def inSpot(letter, word, spot):
     """Returns boolean response if letter is in the given spot in the word."""
-
+    correctLetter = word[spot]
+    if letter == correctLetter:
+        return True
     return False
 
 def rateGuess(myGuess, word):
@@ -17,6 +21,17 @@ def rateGuess(myGuess, word):
     - Capital letter if the letter is in the right spot
     - Lower case letter if the letter is in the word but in the wrong spot
     - * if the letter is not in the word at all"""
+    feedback = ""
+    for spot in range(5):
+        myLetter = myGuess[spot]
+        if inSpot(myLetter, word, spot) == True:
+            feedback = feedback + myLetter.upper()
+        elif inWord(myLetter, word) == True:
+            feedback = feedback + myLetter.lower()
+        else:
+            feedback = feedback + "*"
+
+    return feedback
 
 
 def main():
@@ -25,15 +40,32 @@ def main():
     content = wordFile.read()
     wordList = content.split("\n")
     todayWord = random.choice(wordList)
-    print(todayWord)
+    #print(todayWord)
 
     #User should get 6 guesses to guess
+    guessNum = 1
+    while guessNum <= 6:
+        #Ask user for their guess
+        validWord = False
+        while validWord == False:
+            guess = input("Enter guess: ")
+            guess = guess.lower()
+            if guess not in wordList:
+                print("Word not in list, try again")
+                validWord = False
+            else:
+                validWord = True
 
-    #Ask user for their guess
-    #Give feedback using on their word:
+        #Give feedback using on their word:
+        feedback = rateGuess(guess, todayWord)
+        print(feedback)
+        if feedback == todayWord.upper():
+            print("Congratulations, you guessed the word in " + str(guessNum) + " guesses!")
+            break   
+        
+        guessNum = guessNum + 1
 
-
-
+    print("The word was: " + todayWord)
 
 
 if __name__ == '__main__':
